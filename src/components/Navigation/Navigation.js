dccb6291732c98686dd3c610cd0c891c13d2dfdc
Josh Habdas <jhabdas@gmail.com>
2015-10-16 14:24:05 -0500

    Stateful navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,6 +3,17 @@ import classNames from 'classnames';
 import styles from './Navigation.css';
 import withStyles from '../../decorators/withStyles';
 import Link from '../Link';
+import Location from '../../core/Location';
+
+const INTERNAL_LINKS = [
+  {body: 'Home', href: '/'},
+  {body: 'Schedule', href: '/schedule'},
+  {body: 'Shows', href: '/shows'},
+  {body: 'News', href: '/news'},
+  {body: 'Events', href: '/events'},
+  {body: 'Info', href: '/info'},
+  {body: 'Sponsor', href: '/sponsor'}
+]
 
 @withStyles(styles)
 class Navigation extends Component {
@@ -11,16 +22,53 @@ class Navigation extends Component {
     className: PropTypes.string,
   };
 
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      location: {}
+    }
+  }
+
+  componentDidMount() {
+    this._unlisten = Location.listen((location) => {
+      this.setState({ location });
+    });
+  }
+
+  componentWillUnmount() {
+    this._unlisten();
+  }
+
   render() {
+    let { location } = this.state;
+    let className;
     return (
       <div className={classNames(this.props.className, 'Navigation')}>
-        <a className="Navigation-link Navigation-link--highlight" href="/" onClick={Link.handleClick}>home</a>
-        <a className="Navigation-link" href="/schedule" onClick={Link.handleClick}>schedule</a>
-        <a className="Navigation-link" href="/shows" onClick={Link.handleClick}>shows</a>
-        <a className="Navigation-link" href="/news" onClick={Link.handleClick}>news</a>
-        <a className="Navigation-link" href="/events" onClick={Link.handleClick}>events</a>
-        <a className="Navigation-link" href="/info" onClick={Link.handleClick}>info</a>
-        <a className="Navigation-link" href="/sponsor" onClick={Link.handleClick}>sponsor</a>
+        {INTERNAL_LINKS.map((link, i) => {
+          let { pathname } = location;
+
+          // make link body lowercase if it's a string
+          (typeof link.body === 'string')
+            ? link.body = link.body.toLowerCase()
+            : null;
+
+          // set class names, highlighting the selected link by location
+          className = classNames({
+            'Navigation-link': true,
+            "Navigation-link--highlight": () => {
+              if (!pathname) return;
+              if (link.href === '/') {
+                return link.href === pathname;
+              } else {
+                return pathname.startsWith(link.href);
+              }
+            }()
+          })
+          return (
+            <a key={i} className={className} href={link.href} onClick={Link.handleClick}>{link.body}</a>
+          )
+        })}
         <a className="Navigation-link" href="http://underthecounterculture.bigcartel.com" target="_blank">shop</a>
         <a className="Navigation-link" href="http://www.lumpen.com" target="_blank">lumpen.com</a>
       </div>
