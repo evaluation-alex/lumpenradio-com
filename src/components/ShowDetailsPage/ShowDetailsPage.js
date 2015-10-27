1a07b434edbfb7fb8209ee34e8332970d76856b1
Josh Habdas <jhabdas@gmail.com>
2015-10-27 16:29:23 -0500

    Render Host info with Bios, and Episodes on Show details page

--- src/components/ShowDetailsPage/ShowDetailsPage.js
+++ src/components/ShowDetailsPage/ShowDetailsPage.js
@@ -16,7 +16,8 @@ class ShowDetailsPage extends React.Component {
       hosts: PropTypes.shape({
         artistId: PropTypes.string.isRequired,
         artist: PropTypes.shape({
-          name: PropTypes.string.isRequired
+          name: PropTypes.string.isRequired,
+          bio: PropTypes.string
         })
       })
     }).isRequired
@@ -35,25 +36,18 @@ class ShowDetailsPage extends React.Component {
     let pageTitle = `${show.title} | WLPN`;
     this.context.onSetTitle(pageTitle);
 
-    let showTitle = () => {
-      let { hosts } = show;
-      if (hosts) {
-        let hostNames = hosts.map(
-          host => (host.artist) ? host.artist.name : host.artistId
-        );
-        let artistsString = hostNames.join(' / ');
-        return `${artistsString} \/\/ ${show.title}`;
-      } else return show.title;
-    }();
-
+    // TODO: Add inflection to host header dependent on number of hosts
     return (
       <div className="ShowDetailsPage">
-        <section className="ShowDetailsPage-linksAndInfo">
-          <NewsList title="Links/Info" />
+        <section className="ShowDetailsPage-hostInfo">
+          <header className="ShowDetailsPage-hostInfo__header">
+            <h1>Hosts</h1>
+          </header>
+          {this._renderHostInfo.call(this)}
         </section>
 
         <section className="ShowDetailsPage-about">
-          <header className="ShowDetailsPage-aboutHeader">
+          <header className="ShowDetailsPage-about__header">
             <h1>About</h1>
           </header>
           <ReactCSSTransitionGroup
@@ -62,21 +56,59 @@ class ShowDetailsPage extends React.Component {
             transitionLeaveTimeout={300}
             transitionAppear={true}
             transitionAppearTimeout={500}>
-            <img key={show.id} src={show.logoHref} className="ShowDetailsPage-aboutImage" />
+            <img key={show.id} src={show.logoHref} className="ShowDetailsPage-aboutImage__image" />
           </ReactCSSTransitionGroup>
           <div className="ShowDetailsPage-aboutHeadline">
-            <h2>{showTitle}</h2>
+            <h2>{show.title}</h2>
           </div>
           {(show.description) ? show.description : show.id}
         </section>
 
-        <section className="ShowDetailsPage-episodes">
-          <NewsList title="Episodes" />
-        </section>
+        {this._renderEpisodes.call(this)}
       </div>
     );
   }
 
+  // TODO: Add Read More/Less feature for wordy bios
+  _renderHostInfo() {
+    const { hosts } = this.props.show;
+    if (!hosts) return null;
+    return (
+      <div className="ShowDetailsPage-hostInfoList">
+        {hosts.map((host, i) => {
+          let hostName = (host.artist) ? host.artist.name : host.artistId
+          return (
+            <div key={i} className="ShowDetailsPage-hostInfoItem">
+              <h2>{hostName}</h2>
+              {(host.artist.bio) ? <div>{host.artist.bio}</div> : null}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  _renderEpisodes() {
+    const { episodes } = this.props.show;
+    if (!episodes) return null;
+    return (
+      <section className="ShowDetailsPage-episodes">
+        <header className="ShowDetailsPage-episodes__header">
+          <h1>Episodes</h1>
+        </header>
+        <div className="ShowDetailsPage-episodesList">
+          {episodes.map((episode, i) => {
+            const { title, linkHref } = episode;
+            return (
+              <div key={i} className="ShowDetailsPage-episodesItem">
+                {title} - <a href={linkHref} className="ShowDetailsPage-episodesItem__a">Download</a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
 }
 
 export default ShowDetailsPage;
